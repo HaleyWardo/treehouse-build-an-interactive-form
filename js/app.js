1592be8fe57f427d8ba4ee3c818633ada42cbd51
Haley Ward <hayevans18@gmail.com>
2018-05-14 18:40:56 -0500

    refactor and added docs

--- js/app.js
+++ js/app.js
@@ -26,11 +26,20 @@ let paypal = false;
 let bitcoin = false;
 
 const submitButton = document.querySelector('button');
+let theme = document.querySelector('#theme');
+
+const inputform = document.querySelectorAll('input');
+const legend = document.querySelector('.activities legend');
+let activityError = `<div id="activityError">Please select an activity</div>`;
+activityErrorMessage = false;
 
 otherJobTitle.style.display = 'none';
 
-// Event listener for job role options
-// When job role option is other, display other input field
+/**
+ * Event listener for job role options.
+ * When job role option is other, display other input field.
+ * @param {any} e
+ */
 titleSelect.onchange = (e) => {
   otherJobTitle.style.display = 'none';
   const titleValue = e.target.value.toLowerCase();
@@ -41,19 +50,19 @@ titleSelect.onchange = (e) => {
   }
 }
 
+// Sets default design selection
+if (designSelect.value === 'Select Theme') {
+  punsShirts.remove();
+  heartShirts.remove();
+  heartShirtSelection = true;
+  punsShirtSelection = true;
+  color.style.display = 'none';
+}
 
-//DUPLICATES
-// Event listener for t-shirt design
- if (designSelect.value === 'Select Theme') {
-    punsShirts.remove()
-    heartShirts.remove()
-    heartShirtSelection = true;
-    punsShirtSelection = true;
-    color.style.display = 'none';
-
-  }
-  let theme = document.querySelector('#theme');
-
+/**
+ * Event listener for when the design selection changes
+ * @param {any} e
+ */
 designSelect.onchange = (e) => {
   theme.disabled = true;
   color.style.display = 'inline';
@@ -69,17 +78,19 @@ designSelect.onchange = (e) => {
 
   if (designValue === 'js puns') {
     punsShirtSelection = true;
-    heartShirts.remove()
+    heartShirts.remove();
   } else {
     heartShirtSelection = true;
-    punsShirts.remove()
+    punsShirts.remove();
   }
 }
 
 activitySumHTML = document.createTextNode(`$${activitySum}`);
 
-// Iterates over each activity checkbox and adds a onchange event
-// which calculates the total amount and appends to the DOM
+/**
+ * Iterates over each activity checkbox and adds a onchange event
+ * which calculates the total amount and appends to the DOM
+ */
 activities.forEach((activity) => {
   activity.onchange = (e) => {
     activitiesContainer.appendChild(activitySumHTML);
@@ -91,9 +102,13 @@ activities.forEach((activity) => {
 
     activitySumHTML.nodeValue = `$${activitySum}`;
 
+    const timeRegexMatch = activityString.match(/((Tuesday|Wednesday)\s[0-9]|[0-9][0-9])(am-|pm)/gmi);
+    let activityTimes = timeRegexMatch ? timeRegexMatch.join('') : null;
 
-
-    let activityTimes = activityString.match(/((Tuesday|Wednesday)\s[0-9]|[0-9][0-9])(am-|pm)/gmi).join('');
+    // If no times, return early
+    if (!activityTimes) {
+      return;
+    }
 
     // Iterates over each activity labels and compares clicked checkbox time
     // with other activity times and disables checkbox if activity is at the
@@ -101,6 +116,7 @@ activities.forEach((activity) => {
     let activityParent = document.querySelectorAll('.activities label');
     for (let i = 0; i < activityParent.length; i++) {
       activityParentForm = activityParent[i].textContent;
+
       if (activityParentForm.includes(activityTimes)) {
         if (e.target.checked) {
           activityParent[i].firstChild.disabled = true;
@@ -115,7 +131,6 @@ activities.forEach((activity) => {
 
 // Event listener for payment options
 // Hides fields that don't pertain to that payment option
-// DUPLICATES
 if (paymentSelect.value === 'select_method') {
   paypalContainer.style.display = 'none';
   bitcoinContainer.style.display = 'none';
@@ -127,27 +142,26 @@ if (paymentSelect.value === 'select_method') {
 
 let paymentMethod = document.querySelector('#paymentMethod');
 
+/**
+ * Event listener for payment selection. Renders elements based on current
+ * selection.
+ * @param {any} e
+ */
 paymentSelect.onchange = (e) => {
   paymentMethod.disabled = true;
   let paymentValue = e.target.value;
 
-  if (creditCard) {
-    paypalContainer.style.display = 'block';
-    bitcoinContainer.style.display = 'block';
-    creditCard = false;
+  let paymentMethodSelection = (method, displayValue, container1, container2, state) => {
+    if (method) {
+      container1.style.display = displayValue;
+      container2.style.display = displayValue;
+      method = state;
+    }
   }
 
-  if (paypal) {
-    creditCardContainer.style.display = 'block';
-    bitcoinContainer.style.display = 'block';
-    paypal = false;
-  }
-
-  if (bitcoin) {
-    creditCardContainer.style.display = 'block';
-    paypalContainer.style.display = 'block';
-    bitcoin = false;
-  }
+  paymentMethodSelection(creditCard, 'block', paypalContainer, bitcoinContainer, false);
+  paymentMethodSelection(paypal, 'block', creditCardContainer, bitcoinContainer, false);
+  paymentMethodSelection(bitcoin, 'block', creditCardContainer, paypalContainer, false);
 
   switch (paymentValue) {
     case 'credit card' :
@@ -168,13 +182,12 @@ paymentSelect.onchange = (e) => {
     default :
       break;
   }
-}
+};
 
-const inputform = document.querySelectorAll('input');
-const legend = document.querySelector('.activities legend');
-let activityError = `<div id="activityError">Please select an activity</div>`;
-activityErrorMessage = false;
-
+/**
+ * Submit button event listener. Validates the form inputs and
+ * provides error messages.
+ */
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
   let isChecked = document.querySelectorAll('input:checked').length === 0 ? false : true;
@@ -199,23 +212,10 @@ submitButton.addEventListener('click', (e) => {
     activityErrorMessage = true;
   }
 
-  // let cvvRegex = /^(\d{3})?$/;
-  // let cvv = document.querySelector('#cvv');
-
-
-  // if (cvvRegex.test(cvv.value) === false) {
-  //   cvv.value = ''
-  //   cvv.placeholder = 'Enter 3 digit';
-  //   cvv.style.border = '2px dashed red';
-  // }
-
-
-
   let ccNumber = document.querySelector('#cc-num');
   let zipcode = document.querySelector('#zip');
   let cvv = document.querySelector('#cvv');
   let email = document.querySelector('#mail');
-
 
   let ccNumberRegex = /^[0-9]{13,16}$/;
   let zipcodeRegex = /^(\d{5})?$/;
@@ -234,5 +234,4 @@ submitButton.addEventListener('click', (e) => {
   inputFormat(zipcode, zipcodeRegex, 'Enter 3 digit');
   inputFormat(cvv, cvvRegex, 'Enter 3 digit');
   inputFormat(email, emailRegex, 'Enter a valid email address');
-})
-
+});
